9f8865109aedbe8e850a77ebab42a0ce4b2ee3b7
Tomasz Gajek <tomaszgajek@poczta.onet.pl>
2016-09-23 08:05:37 +0200

    smooth scroll

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 
+	smoothScroll(500);
 
+
+		//smoothscroll	
+		function smoothScroll (duration) {
+			$('h4').on('click', function(event) {
+
+		    	var target = $(this);
+
+		    	if( target.length ) {
+		        	event.preventDefault();
+		        	$('html, body').animate({
+		            	scrollTop: target.offset().top
+		       	 	}, duration);
+		    	}
+			});
+		}
 	
 
 
